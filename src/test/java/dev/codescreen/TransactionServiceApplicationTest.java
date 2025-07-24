f87f367e95953692aec96ae286214026073dd49e
Vraj Patel <vrajjptl@gmail.com>
2025-07-24 17:56:37 -0400

    CI added 1

--- src/test/java/dev/codescreen/TransactionServiceApplicationTest.java
+++ src/test/java/dev/codescreen/TransactionServiceApplicationTest.java
@@ -2,12 +2,14 @@ package dev.codescreen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * Dummy class with a dummy method.
  * Rename this class and method to a name that is more appropriate to your coding test.
  */
 @SpringBootTest
+@ActiveProfiles("test") 
 public class TransactionServiceApplicationTest {
 
   /**
